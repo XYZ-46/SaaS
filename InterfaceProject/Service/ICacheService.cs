fbec0c3b716ad3921f135e18e57bfbe28249cbf7
PC Diksha <admin@admin.com>
2024-01-13 14:54:47 +0700

    remove unsed redisService

--- InterfaceProject/Service/ICacheService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InterfaceProject.Service
-{
-    public interface ICacheService
-    {
-        Task<T> GetDataAsync<T>(string key);
-        Task<bool> SetDataAsync<T>(string key, T value, DateTimeOffset expirationTime);
-        Task<object> RemoveDataAsync(string key);
-    }
-}
