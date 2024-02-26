784e0a9f0313b42e333743325f48e4288f69b928
DESKTOP-TJ7BEQB\djs <admin@admin.com>
2024-02-26 22:18:11 +0700

    blm fix

--- API/Middleware/GlobalExceptionHandler.cs
+++ API/Middleware/GlobalExceptionHandler.cs
@@ -5,28 +5,31 @@ namespace API.Middleware
 {
     public class GlobalExceptionHandler : IExceptionHandler
     {
-        public ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
-            if (exception is null) return ValueTask.FromResult(false);
+            if (exception is null) return false;
 
             switch (exception)
             {
                 case ArgumentException:
                 case HttpRequestException:
                     BaseResponse response = new();
-                    httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    httpContext.Response.StatusCode = StatusCodes.Status417ExpectationFailed;
                     response.errorMessage = exception.Message;
-                    httpContext.Response.WriteAsJsonAsync(exception.Message, cancellationToken).ConfigureAwait(false);
+                    await Task.Run(() =>
+                    {
+                        _ = httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+                    }, cancellationToken);
                     break;
                 default:
                     response = new BaseResponse();
                     httpContext.Response.StatusCode = StatusCodes.Status506VariantAlsoNegotiates;
                     response.errorMessage = "Undefined server Error";
-                    httpContext.Response.WriteAsJsonAsync(JsonSerializer.Serialize(response), cancellationToken);
+                    await httpContext.Response.WriteAsJsonAsync(JsonSerializer.Serialize(response), cancellationToken);
                     break;
             }
 
-            return ValueTask.FromResult(true);
+            return true;
         }
     }
 }
